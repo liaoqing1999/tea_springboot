421ad3b5435398fddeee5d9f6be16ddf6dbe230c
廖青1 <qing.liao1@chinacreator.com>
2020-03-30 22:44:40 +0800

    添加扫描包

--- src/main/java/com/qing/tea/TeaApplication.java
+++ src/main/java/com/qing/tea/TeaApplication.java
@@ -2,8 +2,10 @@ package com.qing.tea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.qing.tea")
+
 public class TeaApplication {
 
     public static void main(String[] args) {
