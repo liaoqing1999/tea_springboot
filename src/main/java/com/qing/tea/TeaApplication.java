cd0a262926626f3c0e2a9578b86061b093cf46dc
廖青1 <qing.liao1@chinacreator.com>
2020-03-30 14:30:28 +0800

    项目初始化

---
+++ src/main/java/com/qing/tea/TeaApplication.java
@@ -0,0 +1,13 @@
+package com.qing.tea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qing/tea/TeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qing.tea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
