b0ae49ca90b8e3fd65437f8792470a2713aa03ca
qing.liao1 <qing.liao1@chinacreator.com>
2020-04-01 17:26:22 +0800

    验证码

--- src/main/java/com/qing/tea/controller/TeaController.java
+++ src/main/java/com/qing/tea/controller/TeaController.java
@@ -1,22 +1,51 @@
 package com.qing.tea.controller;
 
+import com.google.code.kaptcha.Producer;
 import com.qing.tea.service.impl.TeaServiceImpl;
 import com.qing.tea.utils.R;
+import com.qing.tea.utils.VerifyUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 
 @RestController
 @RequestMapping("tea")
 public class TeaController {
     @Resource
     private TeaServiceImpl teaService;
-
+    @Resource
+    private Producer kaptchaProducer;
     @RequestMapping("getAll")
     @ResponseBody
-    public R register(){
+    public R getAll(){
         return R.success(teaService.findAll());
     }
+
+    @RequestMapping("test")
+    @ResponseBody
+    public void test(HttpServletResponse response, HttpServletRequest request) throws IOException {
+        //将图片输出给浏览器
+        HttpSession session=request.getSession();
+
+        //利用图片工具生成图片
+        //第一个参数是生成的验证码，第二个参数是生成的图片
+        Object[] objs = VerifyUtil.createImage();
+        //将验证码存入Session
+        session.setAttribute("imageCode",objs[0]);
+        //将图片输出给浏览器
+        BufferedImage image = (BufferedImage) objs[1];
+        response.setContentType("image/png");
+        OutputStream os = response.getOutputStream();
+        ImageIO.write(image, "png", os);
+    }
+
 }
